a400270bfce0e32757a4895ecc51360b7ebfb578
Matheus Moura <matheusmdeabreu@gmail.com>
2024-06-19 11:29:20 -0300

    Use env variables and create Procfile

--- main.py
+++ main.py
@@ -12,23 +12,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
+import os
 
-
-'''
-Make sure the required packages are installed: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from the requirements.txt for this project.
-'''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+# print(os.environ.get('FLASK_KEY'))
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -260,4 +249,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
